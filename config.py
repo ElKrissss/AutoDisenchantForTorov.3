3ee662a6262ac4468e1c7f19f03baba5824b6824
ElKrissss <97351676+ElKrissss@users.noreply.github.com>
2023-06-07 21:20:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -14,12 +14,14 @@
 
 stealc = 917,49 #Steal coords
 dumpc = 1000,49 #Dump coords
-panickey = "o" # Stop
+panickey = "o" # Stop Key
 movedelay = 4
+
 
 # Disenchan Config
 
-start = 1100 , 620
-end = 1055, 444
+start = 1100 , 620 #First Slot
+end = 1055, 444 #Disenchated Item
 delay = 0.01
 reach = 35
+
